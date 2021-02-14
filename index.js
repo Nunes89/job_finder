acfae84bfe9797c65263d40ed5c6423d36a1cc2e
Nunes <GilsonNunes_dev@outlook.com>
2021-02-14 11:38:36 -0300

    added initial database configuration

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db = require("./db/connection");
+
+db.authenticate()
+  .then(() => {
+    console.log("Database successfully connected.");
+  })
+  .catch((error) => {
+    console.log("There was an error connecting to the database. ", error);
+  });
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
