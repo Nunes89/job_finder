41642092777f8a1b90fe062566ef3f529b9a830c
Nunes <GilsonNunes_dev@outlook.com>
2021-02-15 22:44:02 -0300

    added jobs routes and body-parser configuration

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const db = require("./db/connection");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 db.authenticate()
   .then(() => {
@@ -12,5 +16,7 @@ db.authenticate()
   });
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/jobs", require("./routes/jobs"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
