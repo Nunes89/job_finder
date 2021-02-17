51ec8a11f16f0970b9b798f36c4e87d65ba9ccb0
Nunes <GilsonNunes_dev@outlook.com>
2021-02-17 08:49:55 -0300

    added views and handlebars configuration

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+const handlebars = require("express-handlebars");
 const db = require("./db/connection");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
 
 db.authenticate()
   .then(() => {
@@ -15,7 +22,7 @@ db.authenticate()
     console.log("There was an error connecting to the database. ", error);
   });
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.render("index"));
 
 app.use("/jobs", require("./routes/jobs"));
 
